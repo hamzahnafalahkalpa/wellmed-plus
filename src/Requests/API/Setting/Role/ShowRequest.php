0e106586336c8b69c5639610af349978d629df7d
hamzah <hamzahnafalah@gmail.com>
2025-10-06 12:40:20 +0700

    update for hq

--- src/Requests/API/Setting/Role/ShowRequest.php
+++ src/Requests/API/Setting/Role/ShowRequest.php
@@ -24,7 +24,6 @@ class ShowRequest extends FormRequest
     public function rules(): array
     {  
         return [
-            'id' => ['required','numeric',$this->idValidation('Role')]
         ];
     }
 }
